dd55e3b968ed6ef5b6d4c07dfadf758ee88e9f68
Travis Fields <neoknght@gmail.com>
2018-03-11 21:03:40 -0700

    Updated to include oid and cleanup output
    - Additional logic for checking patterns

--- main.go
+++ main.go
@@ -1,41 +1,79 @@
 package main
 
 import (
+	"bufio"
 	"crypto/x509"
+	"encoding/asn1"
+	"encoding/pem"
+	"fmt"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
 	"os"
-	"bufio"
-	"fmt"
+	"regexp"
 )
 
 var logger *log.Logger
-var config = loadConfig()
+var config AutosignConfig
+
+var subjectNameOid = asn1.ObjectIdentifier{2, 5, 29, 17}
+var dnsAltNames = asn1.ObjectIdentifier{1, 2, 840, 113549, 1, 9, 14}
+
+const configFile = "/etc/puppetlabs/puppet/autosign.yaml"
+const logFile = "puppetlabs-autosign.log"
 
 type AutosignConfig struct {
-	AutosignChallenge string `yaml:"autosign"`
-	LogFile           string `yaml:"logfile"`
+	AutosignChallenge string   `yaml:"challengePassword"`
+	AutosignPatterns  []string `yaml:"autosignPatterns"`
+	LogFile           string   `yaml:"logFile"`
 }
-func info(msg string, int interface{}) {
-	logger.Printf(msg,int)
+
+func info(msg string, int ...interface{}) {
+	fmt.Printf(msg, int...)
+	logger.Printf(msg, int...)
 }
-func check_error(err error){
+func logError(err error, msg string, int ...interface{}) {
+	fmt.Errorf(msg, int...)
+	logger.Fatal(err)
+}
+func pick(file1 string, file2 string) string {
+	if fileExists(file1) {
+		return file1
+	}
+	if fileExists(file2) {
+		return file2
+	}
+	return ""
+}
+
+func checkError(err error) {
 	if err != nil {
-		fmt.Errorf("An error has occured, halting: %s", err)
+		logError(err, "An error has occured, halting: %s", err)
 		panic(err)
 	}
 }
 
+func fileExists(filename string) bool {
+	_, err := os.Stat(filename)
+	return !os.IsNotExist(err)
+}
+
 func loadConfig() AutosignConfig {
-	t := AutosignConfig{}
-	autosign, _ := ioutil.ReadFile("/etc/puppetlabs/puppet/autosign.yaml")
-	if err := yaml.Unmarshal([]byte(autosign), &t); err != nil {
-		logger.Fatalf("Unable to read config file;\n%s", err)
+	t := AutosignConfig{LogFile: logFile}
+	configYaml := pick(configFile, "autosign.yaml")
+	if configYaml != "" {
+		autosign, err := ioutil.ReadFile(configYaml)
+		yaml.Unmarshal(autosign, &t)
+		fmt.Printf("Loaded config map of:\n\tPatterns: %s\n\tLogfile: %s\n\tChallenge: %s\n", t.AutosignPatterns, t.LogFile, t.AutosignChallenge)
+		if err != nil {
+			if err := yaml.Unmarshal([]byte(autosign), &t); err != nil {
+				logger.Fatalf("Unable to read config file;\n%s", err)
+			}
+		}
+	} else {
+		fmt.Println("Unable to read file %s", configYaml)
 	}
-	if t.LogFile == "" {
-		t.LogFile = "/var/log/puppetlabs/autosign.log"
-	}
+	fmt.Printf("Loaded config file %s, with values %s \n", configYaml, t)
 	return t
 }
 
@@ -43,24 +81,78 @@ func readCert() []byte {
 	fileIn := os.Stdin
 	if fileIn != nil {
 		cert, err := ioutil.ReadFile(fileIn.Name())
-		check_error(err)
+		checkError(err)
 		return cert
 	}
 	return []byte{}
 }
 
 func createLogger() {
+	fmt.Println("Creating log")
 	f, err := os.Create(config.LogFile)
-	check_error(err)
+	checkError(err)
 	writer := bufio.NewWriter(f)
-	logger = log.New(writer, "[autosign]",0)
+	defer writer.Flush()
+	logger = log.New(writer, "[autosign]", 1)
+}
+
+func logCertDetails(cr *x509.CertificateRequest) {
+	for i, name := range cr.Subject.Names {
+		info("Name %d: \n\tType: %s\n\tValue: %s\n", i, name, name.Value)
+	}
+	info("Subject: %s \nDNSNames: %s\n", cr.Subject.Names, cr.DNSNames)
+	info("Extensions: \n")
+	for i, ext := range cr.Extensions {
+		if len(ext.Value) != 0 {
+			info("\t%d: %s\n", i, ext)
+		}
+	}
+}
+
+func hostnameMatch(hostname string) bool {
+	for _, pattern := range config.AutosignPatterns {
+		info("Checking pattern '%s'\n", pattern)
+		if _, err := regexp.Compile(pattern); err != nil {
+			info("Failed to compile pattern %s\n\t%s\n", pattern, err)
+		} else {
+			if match, _ := regexp.MatchString(pattern, hostname); match {
+				info("Matching pattern %s for Hostname %s", pattern, hostname)
+			}
+		}
+	}
+	info("Do not sign %s Failed to match any pattern\n", hostname)
+	return false
 }
 
 func main() {
-	hostname := os.Args[0]
+	hostname := os.Args[1]
+	fmt.Printf("Autosign for %s and config file %s\n", hostname, configFile)
+	config = loadConfig()
 	createLogger()
-	info("Checking certificate for %s", hostname)
+	info("Checking certificate for %s \n", hostname)
 	cert := readCert()
-	x509.ParseCertificateRequest(cert)
+	pemCert, _ := pem.Decode(cert)
+	cr, err := x509.ParseCertificateRequest(pemCert.Bytes)
+	cr.Extensions = getPuppetExtensions()
+	if err != nil {
+		logError(err, "Failed to parse certificate request for %s:\n\t%s \n", hostname, err)
+		panic(err)
+	}
+	logCertDetails(cr)
+	if len(cr.DNSNames) == 0 {
+		info("No DNS Alt Names\n")
+		if len(config.AutosignPatterns) == 0 {
+			info("Signing cert for %s: Reason, NO DNS Alt Names matches no pattern match set \n", hostname)
+			os.Exit(0)
+		} else {
+			if hostnameMatch(hostname) {
 
+			} else {
+				os.Exit(1)
+			}
+		}
+	} else {
+		info("Signing cert for %s\n", hostname)
+		os.Exit(0)
+	}
 }
---
+++ puppet_cert_helper.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"crypto/x509/pkix"
+	"encoding/asn1"
+)
+
+var oidPuppetMap = map[string]asn1.ObjectIdentifier{
+	"pp_uuid":             {1, 3, 6, 1, 4, 1, 34380, 1, 1, 1},
+	"pp_instance_id":      {1, 3, 6, 1, 4, 1, 34380, 1, 1, 2},
+	"pp_image_name":       {1, 3, 6, 1, 4, 1, 34380, 1, 1, 3},
+	"pp_preshared_key":    {1, 3, 6, 1, 4, 1, 34380, 1, 1, 4},
+	"pp_cost_center":      {1, 3, 6, 1, 4, 1, 34380, 1, 1, 5},
+	"pp_product":          {1, 3, 6, 1, 4, 1, 34380, 1, 1, 6},
+	"pp_project":          {1, 3, 6, 1, 4, 1, 34380, 1, 1, 7},
+	"pp_application":      {1, 3, 6, 1, 4, 1, 34380, 1, 1, 8},
+	"pp_service":          {1, 3, 6, 1, 4, 1, 34380, 1, 1, 9},
+	"pp_employee":         {1, 3, 6, 1, 4, 1, 34380, 1, 1, 10},
+	"pp_created_by":       {1, 3, 6, 1, 4, 1, 34380, 1, 1, 11},
+	"pp_environment":      {1, 3, 6, 1, 4, 1, 34380, 1, 1, 12},
+	"pp_role":             {1, 3, 6, 1, 4, 1, 34380, 1, 1, 13},
+	"pp_software_version": {1, 3, 6, 1, 4, 1, 34380, 1, 1, 14},
+	"pp_department":       {1, 3, 6, 1, 4, 1, 34380, 1, 1, 15},
+	"pp_cluster":          {1, 3, 6, 1, 4, 1, 34380, 1, 1, 16},
+	"pp_provisioner":      {1, 3, 6, 1, 4, 1, 34380, 1, 1, 17},
+	"pp_region":           {1, 3, 6, 1, 4, 1, 34380, 1, 1, 18},
+	"pp_datacenter":       {1, 3, 6, 1, 4, 1, 34380, 1, 1, 19},
+	"pp_zone":             {1, 3, 6, 1, 4, 1, 34380, 1, 1, 20},
+	"pp_network":          {1, 3, 6, 1, 4, 1, 34380, 1, 1, 21},
+	"pp_securitypolicy":   {1, 3, 6, 1, 4, 1, 34380, 1, 1, 22},
+	"pp_cloudplatform":    {1, 3, 6, 1, 4, 1, 34380, 1, 1, 23},
+	"pp_apptier":          {1, 3, 6, 1, 4, 1, 34380, 1, 1, 24},
+	"pp_hostname":         {1, 3, 6, 1, 4, 1, 34380, 1, 1, 25},
+}
+
+func getPuppetExtensions() []pkix.Extension {
+	exts := []pkix.Extension{}
+	for k := range oidPuppetMap {
+		exts = append(exts, pkix.Extension{Id: oidPuppetMap[k]})
+	}
+	return exts
+}
